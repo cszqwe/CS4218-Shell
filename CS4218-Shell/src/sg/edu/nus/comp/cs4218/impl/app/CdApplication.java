c2886914621f5efd9fc68c9c7a6fa57cf7e52b60
Hyung Jon Kim <hyungjon.kim@gmail.com>
2017-02-08 11:59:06 +0800

    Implemented CD

--- CS4218-Shell/src/sg/edu/nus/comp/cs4218/impl/app/CdApplication.java
+++ CS4218-Shell/src/sg/edu/nus/comp/cs4218/impl/app/CdApplication.java
@@ -2,6 +2,7 @@ package sg.edu.nus.comp.cs4218.impl.app;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.File;
 
 import sg.edu.nus.comp.cs4218.Application;
 import sg.edu.nus.comp.cs4218.Environment;
@@ -11,9 +12,14 @@ public class CdApplication implements Application {
 
 	@Override
 	public void run(String[] args, InputStream stdin, OutputStream stdout) throws AbstractApplicationException {
-		// TODO: implement the case when input is just "cd": we need to go to parent dir
-		// TODO: check if dir actually exists
-		Environment.currentDirectory = Environment.currentDirectory + '\\' + args[0];
+		String path = Environment.currentDirectory + '\\' + args[0];
+		File f = new File(path);
+		if (f.exists() && f.isDirectory()) {
+			Environment.currentDirectory = path;
+		} else {
+			System.out.println("Directory not found");
+		}
+		
 	}
 
 }

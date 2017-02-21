04f056fd23b684552e032f4b207e9e85213498ad
cszqwe <183223660@qq.com>
2017-02-21 23:10:09 +0800

    Finish the stdin part of WcApplication

--- CS4218-Shell/src/sg/edu/nus/comp/cs4218/impl/app/WcApplication.java
+++ CS4218-Shell/src/sg/edu/nus/comp/cs4218/impl/app/WcApplication.java
@@ -1,7 +1,9 @@
 package sg.edu.nus.comp.cs4218.impl.app;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,9 +16,13 @@ import sg.edu.nus.comp.cs4218.exception.OutputstreamNotValidException;
 import sg.edu.nus.comp.cs4218.exception.WcException;
 
 public class WcApplication implements Wc {
-
+	InputStream stdin;
+	OutputStream stdout;
+	
 	@Override
 	public void run(String[] args, InputStream stdin, OutputStream stdout) throws WcException, OutputstreamNotValidException {
+		this.stdin = stdin;
+		this.stdout = stdout;
 		ArrayList<String> files = new ArrayList<>();
 		boolean chars = false;
 		boolean words = false;
@@ -28,41 +34,38 @@ public class WcApplication implements Wc {
 				throw new WcException("Null Pointer Exception");
 			}
 		}
-		if (args.length == 0) {
-			// read from stdin, count all
-		} else {
-			for (String arg : args) {
-				if (arg.charAt(0) == '-') { // indicates option
-					char invalidOption = 0;
-					for (int i = 1; i < arg.length(); i++) {
-						if (arg.charAt(i) == 'm') {
-							chars = true;
-						} else if (arg.charAt(i) == 'w') {
-							words = true;
-						} else if (arg.charAt(i) == 'l') {
-							lines = true;
-						} else {
-							invalidOption = arg.charAt(i);
-							throw new WcException("invalid option -- " + invalidOption);
-						}
+		for (String arg : args) {
+			if (arg.charAt(0) == '-') { // indicates option
+				char invalidOption = 0;
+				for (int i = 1; i < arg.length(); i++) {
+					if (arg.charAt(i) == 'm') {
+						chars = true;
+					} else if (arg.charAt(i) == 'w') {
+						words = true;
+					} else if (arg.charAt(i) == 'l') {
+						lines = true;
+					} else {
+						invalidOption = arg.charAt(i);
+						throw new WcException("invalid option -- " + invalidOption);
 					}
-				} else {
-					files.add(arg); // first add, later check for validity
-				}
-			}
-			if (!lines && !words && !chars) { // no options selected: set to default count all
-				chars = true;
-				words = true;
-				lines = true;
-			}
-			if (files.size() > 0) {
-				// read from file(s), even if stdin is provided
-				for (String file : files) {
-					output = output.concat(printCountInFile(file, chars, words, lines));
 				}
 			} else {
-				// read from stdin
+				files.add(arg); // first add, later check for validity
 			}
+		}
+		if (!lines && !words && !chars) { // no options selected: set to default count all
+			chars = true;
+			words = true;
+			lines = true;
+		}
+		if (files.size() > 0) {
+			// read from file(s), even if stdin is provided
+			for (String file : files) {
+				output = output.concat(printCountInFile(file, chars, words, lines));
+			}
+		} else {
+			// read from stdin
+			output = output.concat(printCountInStdin(stdin));
 		}
 		try {
 			stdout.write(output.getBytes());
@@ -94,9 +97,44 @@ public class WcApplication implements Wc {
 		
 		return line + "\n";
 	}
+	
+	public static String readInputStreamToString(InputStream stdin) {      
+         BufferedReader in = new BufferedReader(new InputStreamReader(stdin));
+         StringBuilder str = new StringBuilder();      
+         String line = null; 
+         boolean first = true;
+        try {
+        	while ((line = in.readLine()) != null) {
+        		if (!first)
+        			str.append("\n" + line);      
+        		else {
+        			str.append(line);
+        			first = false;
+        		}
+        	}      
+         } catch (IOException e) {      
+             e.printStackTrace();      
+         } finally {      
+            try {      
+                 in.close();      
+             } catch (IOException e) {      
+                 e.printStackTrace();      
+             }      
+         }
+        return str.toString();
+     }
+	public String printCountInStdin(InputStream stdin) {
+		String line = "";
+		String contents = readInputStreamToString(stdin);
+		line = line.concat(printCharacterCountInStdin(contents) + " ");
+		line = line.concat(printWordCountInStdin(contents) + " ");
+		line = line.concat(printNewlineCountInStdin(contents) + " ");
+		return line + "\n";
+	}
 
 	@Override
 	public String printCharacterCountInFile(String args) {
+		args.replaceAll("\r\n", "\n");
 		return Integer.toString(args.length());
 	}
 
@@ -115,32 +153,38 @@ public class WcApplication implements Wc {
 
 	@Override
 	public String printAllCountsInFile(String args) {
-		// TODO Auto-generated method stub
-		return null;
+		String line = "";
+		line = line.concat(printCharacterCountInFile(args) + " ");
+		line = line.concat(printWordCountInFile(args) + " ");
+		line = line.concat(printNewlineCountInFile(args) + " ");
+		return line;
 	}
 
 	@Override
 	public String printCharacterCountInStdin(String args) {
-		// TODO Auto-generated method stub
-		return null;
+		return Integer.toString(args.length());
 	}
 
 	@Override
 	public String printWordCountInStdin(String args) {
-		// TODO Auto-generated method stub
-		return null;
+		String trim = args.trim();
+		if (trim.isEmpty()) return "0";
+	    return Integer.toString(trim.split("\\s+").length);
 	}
 
 	@Override
 	public String printNewlineCountInStdin(String args) {
-		// TODO Auto-generated method stub
-		return null;
+		if (args.equals("")) return "0";
+		return Integer.toString(args.split("\n", -1).length);
 	}
 
 	@Override
 	public String printAllCountsInStdin(String args) {
-		// TODO Auto-generated method stub
-		return null;
+		String line = "";
+		line = line.concat(printCharacterCountInFile(args) + " ");
+		line = line.concat(printWordCountInFile(args) + " ");
+		line = line.concat(printNewlineCountInFile(args) + " ");
+		return line;
 	}
 
 }
